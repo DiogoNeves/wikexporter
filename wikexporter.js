6733247340ce8ba57fa63ff10ef5f5a940c17c63
Diogo Neves <diogo.neves@gmail.com>
2012-10-14 03:37:47 +0100

    Added repo argument

--- wikexporter.js
+++ wikexporter.js
@@ -1,1 +1,19 @@
+#!/usr/bin/env node
+var argv = require('optimist')
+	.options({
+		// All option keys
+		'r': {
+			alias: 'repo',
+			demand: true,
+			describe: 'Repository where to grab the wiki from (e.g. https://github.com/DiogoNeves/wikexporter.git)'
+		}
+	})
+	.argv
+;
 
+var Git = require('git-wrapper');
+var git = new Git();
+
+// validate this is a repo
+
+// grab from the github repo provided
