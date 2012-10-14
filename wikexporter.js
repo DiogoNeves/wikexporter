4bae6a6a10ebd04b66782afa19c05f864aa8e0e4
Diogo Neves <diogo.neves@gmail.com>
2012-10-14 04:15:24 +0100

    Added repo validation and wiki repo transform

--- wikexporter.js
+++ wikexporter.js
@@ -1,19 +1,49 @@
 #!/usr/bin/env node
-var argv = require('optimist')
+var optimist = require('optimist')
 	.options({
 		// All option keys
 		'r': {
 			alias: 'repo',
 			demand: true,
-			describe: 'Repository where to grab the wiki from (e.g. https://github.com/DiogoNeves/wikexporter.git)'
+			describe: 'GitHub (only) repo where to grab the wiki from (e.g. https://github.com/DiogoNeves/wikexporter.git)'
 		}
-	})
-	.argv
+	});
 ;
+var argv = optimist.argv;
 
+function argv_fail(msg) {
+	optimist.showHelp();
+	if (msg)
+		console.error('Problem: ' + msg);
+	process.exit(1);
+}
+
+function fail(msg) {
+	if (msg)
+		console.error('Fatal: ' + msg);
+	process.exit(2);
+}
+
+// validate this is a repo
+var url = require('url').parse(argv.repo);
+if (url.hostname !== 'github.com') argv_fail('It must be a GitHub repo!');
+if (url.query) argv_fail('Please, only plain repo urls, no queries ;)');
+if (url.href.lastIndexOf('.git') !== (url.href.length - '.git'.length)) argv_fail('That isn\'t a git repo... is it?');
+
+// generate the wiki repo from the original repo
+// we use the lastIndexOf because it was validated above
+var wikiRepo = argv.repo.substring(0, url.href.lastIndexOf('.git'));
+wikiRepo += '.wiki.git';
+
+// grab from the github repo provided
 var Git = require('git-wrapper');
 var git = new Git();
 
-// validate this is a repo
-
-// grab from the github repo provided
+var commandArgs = wikiRepo + ' wiki/';
+console.log('Grabbing wiki repo from "' + wikiRepo + '"');
+//git.exec('clone', commandArgs, function(err, stdout) {
+//	if (err)
+//		fail('Failed to clone the wiki repo "' + wikiRepo + '\r\n' + stdout);
+//	
+//	// convert .md files to html
+//});
